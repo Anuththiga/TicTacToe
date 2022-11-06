1cb634fe24712f360b5c3684404df8036d128272
Tanuththiga <anuththiga25jt@gmail.com>
2022-11-06 11:31:56 +0530

    gameboard added in

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,62 +1,115 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
+	
+	static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
+	static ArrayList<Integer> computerPositions = new ArrayList<Integer>();
+
 
 	public static void main(String[] args) {
-		char [][] gameBoard = {
-				{' ', '|', ' ', '|', ' '},
-				{'-', '+', '-', '+', '-'},
-				{' ', '|', ' ', '|', ' '},
-				{'-', '+', '-', '+', '-'},
-				{' ', '|', ' ', '|', ' '},
-		};
+		char[][] gameBoard = { { ' ', '|', ' ', '|', ' ' }, { '-', '+', '-', '+', '-' }, { ' ', '|', ' ', '|', ' ' },
+				{ '-', '+', '-', '+', '-' }, { ' ', '|', ' ', '|', ' ' }, };
 		printGameBoard(gameBoard);
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the Position(1-9): ");
-		int position = sc.nextInt();
-		
-		System.out.println(position);
-		
-		switch(position) {
-		case 1:
-			gameBoard[0][0] = 'X';
-			break;
-		case 2:
-			gameBoard[0][2] = 'X';
-			break;
-		case 3:
-			gameBoard[0][4] = 'X';
-			break;
-		case 4:
-			gameBoard[2][0] = 'X';
-			break;
-		case 5:
-			gameBoard[2][2] = 'X';
-			break;
-		case 6:
-			gameBoard[2][4] = 'X';
-			break;
-		case 7:
-			gameBoard[4][0] = 'X';
-			break;
-		case 8:
-			gameBoard[4][2] = 'X';
-			break;
-		case 9:
-			gameBoard[4][4] = 'X';
-			break;
+		while(true) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the Position(1-9): ");
+			int position = sc.nextInt();
+
+			System.out.println(position);
+			
+			piecePlace(gameBoard, position, "Player");
+			
+			Random random = new Random();
+			int compPosition = random.nextInt(9) + 1;
+			
+			piecePlace(gameBoard, compPosition, "Computer");
+			
+			printGameBoard(gameBoard);
 		}
-		printGameBoard(gameBoard);
+
+		
 	}
-	
+
 	public static void printGameBoard(char[][] gameBoard) {
-		for(char[] row : gameBoard) {
-			for(char c : row) {
+		for (char[] row : gameBoard) {
+			for (char c : row) {
 				System.out.print(c);
 			}
 			System.out.println();
 		}
+	}
+
+	public static void piecePlace(char[][] gameBoard, int position, String user) {
+		char symbol = ' ';
+		if(user.equals("Player")) {
+			symbol = 'X';
+		} else if(user.equals("Computer")) {
+			symbol = 'O';
+		}
+		
+		switch (position) {
+		case 1:
+			gameBoard[0][0] = symbol;
+			break;
+		case 2:
+			gameBoard[0][2] = symbol;
+			break;
+		case 3:
+			gameBoard[0][4] = symbol;
+			break;
+		case 4:
+			gameBoard[2][0] = symbol;
+			break;
+		case 5:
+			gameBoard[2][2] = symbol;
+			break;
+		case 6:
+			gameBoard[2][4] = symbol;
+			break;
+		case 7:
+			gameBoard[4][0] = symbol;
+			break;
+		case 8:
+			gameBoard[4][2] = symbol;
+			break;
+		case 9:
+			gameBoard[4][4] = symbol;
+			break;
+		default:
+			break;
+		}
+	}
+	
+	public static String checkWinning() {
+		
+		List topRow = Arrays.asList(1,2,3);
+		List middleRow = Arrays.asList(4,5,6);
+		List bottomRow = Arrays.asList(7,8,9);
+		List leftCol = Arrays.asList(1,4,7);
+		List middleCol = Arrays.asList(2,5,8);
+		List rightCol = Arrays.asList(3,6,9);
+		List cross1 = Arrays.asList(1,5,9);
+		List cross2 = Arrays.asList(3,5,7);
+		
+		List<List> winning = new ArrayList<List>();
+		winning.add(topRow);
+		winning.add(middleRow);
+		winning.add(bottomRow);
+		winning.add(leftCol);
+		winning.add(middleCol);
+		winning.add(rightCol);
+		winning.add(cross1);
+		winning.add(cross2);
+		
+	
+
+		
+		return "";
 	}
 
 }
